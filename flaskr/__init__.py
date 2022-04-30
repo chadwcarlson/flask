8fde6a5983713690156e975fb75629e2239cb6d1
chadcarlson <chad.carlson@platform.sh>
2022-04-30 11:21:29 -0600

    one app.

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,10 +1,23 @@
 import os
-
+import environ
 from flask import Flask
+from pathlib import Path
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+
+# Take environment variables from .env file if present.
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
+
+    
+
+
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
